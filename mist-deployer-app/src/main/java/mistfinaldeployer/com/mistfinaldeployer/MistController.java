2566a024695620b3252d9d4340b03c8d392b8ce0
isaac agaba <akaizat1@gmail.com>
2017-01-14 18:32:20 +0200

    added docker host to the deployer compose file

--- mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
+++ mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
@@ -22,6 +22,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,6 +44,8 @@ public class MistController {
 
     Boolean mistStarted = false;
     String startRequest="";
+    @Value("${localhost}")
+    String host;
     @Autowired
     private HttpServletRequest request;
     @RequestMapping("/stop")
@@ -58,9 +61,10 @@ public class MistController {
     private  void sendRequest() throws IOException {
 
         String postText = startRequest;
+        System.out.println(host);
         System.out.println("Post request sent with this data "+postText);
 
-        String       postUrl       = "http://dockerhost:8080/engine-rest/message";// put in your url
+        String       postUrl       = "http://"+host+":8080/engine-rest/message";// put in your url
         Gson gson          = new Gson();
         HttpClient httpClient    = HttpClientBuilder.create().build();
         HttpPost post          = new HttpPost(postUrl);
@@ -149,7 +153,7 @@ public class MistController {
 
     private  String deploy() throws ClientProtocolException, IOException {
         if(mistpath!=null){
-            String url = "http://dockerhost:8080/manager/text/deploy?path=/mistBpmn&update=true";
+            String url = "http://${dockerhost}:8080/manager/text/deploy?path=/mistBpmn&update=true";
             // get this war generated from the maveen install of the mist-bpmn war
 
 
@@ -179,7 +183,7 @@ public class MistController {
 
     public  String undeploy() throws ClientProtocolException, IOException{
         credsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials("tomcat", "tomcat"));
-        String url = "http://dockerhost:8080/manager/text/undeploy?path=/mistBpmn";
+        String url = "http://${dockerhost}:8080/manager/text/undeploy?path=/mistBpmn";
         HttpGet req = new HttpGet(url) ;
         String response = executeRequest (req, credsProvider);
         System.out.println("Response : "+response);
