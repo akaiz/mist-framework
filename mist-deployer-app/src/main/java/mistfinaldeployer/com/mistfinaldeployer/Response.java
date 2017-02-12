335bd39d18cebe1645849d93573787e1a242246e
isaac agaba <akaizat1@gmail.com>
2017-02-12 23:38:18 +0200

    File update

---
+++ mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/Response.java
@@ -0,0 +1,20 @@
+package mistfinaldeployer.com.mistfinaldeployer;
+
+public class Response<T> {
+    /** Boolean indicating if request succeeded **/
+    private boolean status;
+
+    /** Message indicating error if any **/
+    private String message;
+
+    /** Additional data that is part of this response **/
+    private T data;
+
+    public Response(boolean status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+
+}
