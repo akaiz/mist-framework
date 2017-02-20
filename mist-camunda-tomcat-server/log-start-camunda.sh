8deebb15d5f302680b6cdb6bb6786b2197cf5aa3
isaac agaba <akaizat1@gmail.com>
2017-02-20 15:45:34 +0200

    Added camunda server

--- mist-camunda-tomcat-server/log-start-camunda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export CATALINA_HOME="$(dirname "$0")/server/apache-tomcat-8.0.24"
-echo "starting camunda BPM platform on Tomcat Application Server"
-
-/bin/sh "$(dirname "$0")/server/apache-tomcat-8.0.24/bin/startup.sh"
-
-tail -f server/apache-tomcat-8.0.24/logs/catalina.out
