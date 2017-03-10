1492da27094e057b576c426b1f7aa3327a99f13c
isaac agaba <akaizat1@gmail.com>
2017-03-10 15:34:26 +0200

    Call back added

--- mist-tomcat-app-war/src/main/java/ee/ut/mobilelab/mistbpmn/CallBack.java
+++ mist-tomcat-app-war/src/main/java/ee/ut/mobilelab/mistbpmn/CallBack.java
@@ -1,6 +1,11 @@
 package ee.ut.mobilelab.mistbpmn;
 
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -19,9 +24,16 @@ public class CallBack extends DockerCommands implements JavaDelegate {
         String callBackUrl = (String)execution.getVariable("call_back_url");
         if(callBackUrl!=null){
             LOGGER.info("Sending response to callback "+callBackUrl);
-            LOGGER.info("Sending response data"+execution.getVariable("response"));
-        }
+
+
+            HttpClient httpClient    = HttpClientBuilder.create().build();
+            HttpPost post          = new HttpPost(callBackUrl);
+            StringEntity postingString = new StringEntity("task finshed","UTF-8");//gson.tojson() converts your pojo to json
+            post.setEntity(postingString);
+            post.setHeader("Content-type", "application/json");
+            HttpResponse response2 = httpClient.execute(post);
+            LOGGER.info("Response from the call back"+response2);
 
 
     }
-}
+}}
