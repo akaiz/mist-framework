e637be931fc23a4a7bcbaf0e1ba080d4565111af
isaac agaba <akaizat1@gmail.com>
2017-03-14 03:05:18 +0200

    Mist update

--- mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
+++ mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
@@ -575,10 +575,30 @@ public class MistController {
         return result;
     }
     public static String getIpAddress() throws IOException {
+        String ip=null;
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                // filters out 127.0.0.1 and inactive interfaces
+                if (iface.isLoopback() || !iface.isUp())
+                    continue;
 
-        URL url = new URL("http://checkip.amazonaws.com/");
-        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-        return br.readLine();
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while(addresses.hasMoreElements()) {
+                    InetAddress addr = addresses.nextElement();
+
+                    // *EDIT*
+                    if (addr instanceof Inet6Address) continue;
+
+                    ip = addr.getHostAddress();
+                    System.out.println(iface.getDisplayName() + " " + ip);
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+        return ip;
     }
 
 }
