837c7215d2e4020e0da1e7d4b439fa64f66d2756
isaac agaba <akaizat1@gmail.com>
2017-03-27 21:30:48 +0300

    mist update

--- mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
+++ mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
@@ -489,7 +489,7 @@ public class MistController {
             meb.addTextBody("processId",processId);
             System.out.println("payload ------->"+node.getPayload());
             if(node.getPayload().equals("true")){
-                File mist_payload = new File(mistFilesPath+(node.getMist_file().contains("light_1.txt")?"payload-light.jpg":"pay.jpg"));
+                File mist_payload = new File(mistFilesPath+(node.getMist_file().contains("light_1")?"payload-light.jpg":"payload-heavy.jpeg"));
                 System.out.println("payload ------->"+mist_payload.getAbsolutePath());
                 meb.addBinaryBody("payload", mist_payload, ContentType.APPLICATION_OCTET_STREAM, mist_payload.getName());
             }
