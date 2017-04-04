a4df1b4d8c8a6cbb56741295d32c01bdea272ded
isaac agaba <akaizat1@gmail.com>
2017-04-04 16:18:55 +0300

    mist update

--- mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
+++ mist-deployer-app/src/main/java/mistfinaldeployer/com/mistfinaldeployer/MistController.java
@@ -140,7 +140,7 @@ public class MistController {
     long startTime ,endTime;
     Boolean mistStarted = false;
     String startRequest="";
-    String localhost="localhost";
+    String localhost="http://localhost";
     @Autowired
     private HttpServletRequest request;
 
