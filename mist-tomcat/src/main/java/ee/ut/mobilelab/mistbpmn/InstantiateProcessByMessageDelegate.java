e130e52d94996b833aa0ec683bfc729e35c70e6d
isaac agaba <akaizat1@gmail.com>
2017-04-10 22:17:31 +0300

    bpmn update

--- mist-tomcat/src/main/java/ee/ut/mobilelab/mistbpmn/InstantiateProcessByMessageDelegate.java
+++ mist-tomcat/src/main/java/ee/ut/mobilelab/mistbpmn/InstantiateProcessByMessageDelegate.java
@@ -6,13 +6,13 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-@ProcessApplication("Ut Mist Final App")
+@ProcessApplication("UT Mist Camunda App")
 public class InstantiateProcessByMessageDelegate extends ServletProcessApplication implements JavaDelegate {
     public InstantiateProcessByMessageDelegate() {
     }
 
     public void execute(DelegateExecution execution) throws Exception {
         RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
-        runtimeService.startProcessInstanceByMessage("instantiationMessageMistFinal");
+        runtimeService.startProcessInstanceByMessage("instantiationMessageOpenHab");
     }
 }
